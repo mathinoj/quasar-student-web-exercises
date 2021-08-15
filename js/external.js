9aed7f98358b30d157da2a27d2e7e688da9fbfc1
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-08-15 14:18:33 -0500

    Temporarily completed last exercise for External. Remember to paste the code you removed for convienence sake

--- js/external.js
+++ js/external.js
@@ -1,52 +1,25 @@
 "use strict";
 
-alert("Welcome to my Website!")
 
-var userInputColor = prompt("What is your favorite color?");
-alert("Great, " +userInputColor+ " is my favorite color too.");
+alert ("Buy 2 get 1 free could apply to you! If you are a premium member this offer automatically applies. Regardless, please answer the following questions to see if you qualify for this offer.");
+var productOffer = confirm("This offer expires on October 31, 2021. Has this date passed? Click 'Ok' if October 31, 2021 has passed.");
+    let offerResponse;
+        if (productOffer == null || productOffer == true){
+        alert("The offer has expired.");
+    } else {
+            offerResponse = confirm("Are you a premium member? Click 'Ok' if you are a premium member.");
+            if (productOffer == null || productOffer == true) {
+                alert("This offer applies to you.");
+            } else {
+                alert("Buy 2, get 1 offer applies to you.")
+                offerResponse = confirm("Would you like to become a premium member? Click 'Ok' if you would like to become a premium member.");
+                }
+            }
+            let offerPremium;
+            if (offerPremium == null || offerPremium == true) {
+                offerPremium = prompt("What is your email address? Please type below:");
+                offerResponse = alert("A premium membership application will be sent to: " + offerPremium + "");
+            } else {
+                offerResponse = alert("You have opted out of premium membership. Thank you.")
 
-
-var userInputMovie = prompt("How many days in total did you rent The Little Mermaid, Brother Bear, and Hercules?")
-var userInputCheckOutDate = prompt("Okay, and what is the price for a movie per day? (do not include $)")
-var total = userInputMovie*userInputCheckOutDate
-alert("Your total cost for the movies you rented is " +"$"+ +total+ "");
-
-var userInputGoogle = prompt("How much do you get paid per hour at Google?");
-var userInputAmazon = prompt("How much do you get paid per hour at Amazon?");
-var userInputFacebook = prompt("How much do you get paid per hour at Facebook?");
-
-var userInputGoogleHours = prompt("How many hours did you work this week at Google?");
-var userInputAmazonHours = prompt("How many hours did you work this week at Amazon?");
-var userInputFacebookHours = prompt("How many hours did you work this week at Facebook?");
-
-var totalGoogle = userInputGoogle*userInputGoogleHours
-var totalAmazon = userInputAmazon*userInputAmazonHours
-var totalFacebook = userInputFacebook*userInputFacebookHours
-alert("Your total income this week for working at Google, Amazon, and Facebook is " +"$"+ +(totalGoogle+totalAmazon+totalFacebook)+ "");
-
-
-// When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
-// Use an alert to show the results of each problem.
-
-//A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with
-//her current schedule.
-
-let isClassFull = confirm ("Is the class you are wanting to be enrolled for full? Click 'Ok' if the class is full.");
-let response;
-    if (isClassFull == null || isClassFull == true){
-        response = alert("You cannot be enrolled into the class because it is full.");
-    } else{
-        response = alert("You might be enrolled for the class if it does not conflict with your current schedule.");
-    }
-
-let classSchedule = confirm("Does this class conflict with your current schedule? Click 'Ok' if the class conflicts with your current schedule.");
-    let followUpResponse;
-    if (classSchedule == null || classSchedule == true) {
-        followUpResponse = alert("You cannot be enrolled into the class because it conflicts with your schedule");
-    }else{
-        followUpResponse = alert("You can be enrolled for this class because it does not conflict with your schedule and it is not full");
-    }
-
-console.log(isClassFull)
-console.log(classSchedule)
-
+        }
--- js/function-lec.js
+++ js/function-lec.js
@@ -60,3 +60,55 @@
 //below is an immediately invoked function, which we added to the function above
 //(function () {
 // })();
+
+
+//
+alert("Welcome to my Website!")
+
+var userInputColor = prompt("What is your favorite color?");
+alert("Great, " +userInputColor+ " is my favorite color too.");
+
+
+var userInputMovie = prompt("How many days in total did you rent The Little Mermaid, Brother Bear, and Hercules?")
+var userInputCheckOutDate = prompt("Okay, and what is the price for a movie per day? (do not include $)")
+var total = userInputMovie*userInputCheckOutDate
+alert("Your total cost for the movies you rented is " +"$"+ +total+ "");
+
+var userInputGoogle = prompt("How much do you get paid per hour at Google?");
+var userInputAmazon = prompt("How much do you get paid per hour at Amazon?");
+var userInputFacebook = prompt("How much do you get paid per hour at Facebook?");
+
+var userInputGoogleHours = prompt("How many hours did you work this week at Google?");
+var userInputAmazonHours = prompt("How many hours did you work this week at Amazon?");
+var userInputFacebookHours = prompt("How many hours did you work this week at Facebook?");
+
+var totalGoogle = userInputGoogle*userInputGoogleHours
+var totalAmazon = userInputAmazon*userInputAmazonHours
+var totalFacebook = userInputFacebook*userInputFacebookHours
+alert("Your total income this week for working at Google, Amazon, and Facebook is " +"$"+ +(totalGoogle+totalAmazon+totalFacebook)+ "");
+
+
+// When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
+// Use an alert to show the results of each problem.
+
+//A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with
+//her current schedule.
+
+let isClassFull = confirm ("Is the class you are wanting to be enrolled for full? Click 'Ok' if the class is full.");
+let response;
+if (isClassFull == null || isClassFull == true){
+    response = alert("You cannot be enrolled into the class because it is full.");
+} else{
+    response = alert("You might be enrolled for the class if it does not conflict with your current schedule.");
+}
+
+let classSchedule = confirm("Does this class conflict with your current schedule? Click 'Ok' if the class conflicts with your current schedule.");
+let followUpResponse;
+if (classSchedule == null || classSchedule == true) {
+    followUpResponse = alert("You cannot be enrolled into the class because it conflicts with your schedule");
+}else{
+    followUpResponse = alert("You can be enrolled for this class because it does not conflict with your schedule and it is not full");
+}
+
+console.log(isClassFull)
+console.log(classSchedule)
