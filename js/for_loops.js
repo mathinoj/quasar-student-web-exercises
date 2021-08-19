def9d6517f4ba6d443cbd7778d3d0b3d1aaf7509
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-08-18 23:37:57 -0500

    Finally figured out exercise one of Loops exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,13 +3,30 @@
 
 //Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
 //For example, showMultiplicationTable(7) should output
-
-
-
-function showMultiplicationTable(num){
-    for (num = 1; num === 10; num) {
-        console.log("Howdy our current number is: " + num);
-    }
+//
+//
+function showMultiplicationTable(num) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(+num +" x " +i +" = "  +(i * num));
+        }
 }
+showMultiplicationTable(10);
 
-showMultiplicationTable(6);
+
+
+
+// var i = 0
+// while (i <= 10){
+//     console.log("Currently the variable I is : " +i);
+//     i++;
+// }
+
+//
+// for (i = 1; i <= 10; i++) {
+//     console.log("Howdy our current number is: " + num);
+
+// - incrementer initialization - Here we are declaring the variable for our incrementer, as well as its value.
+
+// - condition - The loop will continue to run until this condition is no longer true.
+
+// - incrementer change - what happens to the incrementer in once the loop has finished executing the code for a given iteration.
