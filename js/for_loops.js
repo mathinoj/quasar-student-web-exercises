a3b650280bf9cd67f28e988bd389425c2e91223a
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-08-19 22:24:16 -0500

    Feat: worked on -5 loop exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -17,16 +17,99 @@
 // var randomNumber = Math.floor(Math.random() * 180) + 20;
 // console.log(randomNumber);
 
-for (var i = 0; i < 10; i++) {
-var randomNumber = Math.floor(Math.random() * 180) + 20;
-console.log(randomNumber);
+// for (var i = 0; i < 10; i++) {
+// var randomNumber = Math.floor(Math.random() * 180) + 20;
+// console.log(randomNumber);
+//
+//     if (randomNumber % 2 === 0){
+//         console.log("Number is even.")
+//     } else {
+//         console.log("Number is odd.")
+//     }
+// }
 
-    if (randomNumber % 2 === 0){
-        console.log("Number is even.")
-    } else {
-        console.log("Number is odd.")
-    }
+//Create a for loop that uses console.log to create the output shown below.
+//
+// var numberStopA = 1;
+// var numberStopB = 22;
+// var numberStopC = 333;
+// var numberStopD = 4444;
+// var numberStopE = 55555;
+// var numberStopF = 666666;
+// var numberStopG = 7777777;
+// var numberStopH = 88888888;
+// var numberStopZ = 999999999;
+//
+//
+// function numberPyramid() {
+// //      for (var i = 1, b = 2; i > 1, b < 10; i++, b++) {
+// //      for (var i = 1; i < 10; i++) {
+// //        var totalNumberOfRows = 9;
+//         var output = '';
+//         for (var i = 1; i <= 9; i++) {
+//                 for (var j = 1; j <= i; j++) {
+//                         output += j + '  ';
+//                 }
+//                 console.log(output);
+//                 output = '';
+//         }
+//
+// numberPyramid();
+
+
+//        console.log(+i, b);
+//        console.log(numberStopB , +numberStopC);
+//        if (i === numberStopZ) {
+//           break;
+//       }
+//      for (var b = 2; b < 10; b++) {
+//        console.log(b);
+//      }
+//    }
+//}
+
+//for (var i = 0, j = 9; i < 10; i++, j--) {
+//     console.log('for loop iteration #' + i + ', j = ' + j);
+// }
+
+
+
+//var numberToStopAt = 5;
+//
+// for (var i = 1; i < 100; i++) {
+//
+//     console.log('Loop counter is: ' + i);
+//
+//     if (i === numberToStopAt) {
+//         console.log('We have reached the stopping point: break!');
+//         // use the break keyword to exit from the while loop
+//         break;
+//         // nothing after the break will get processed
+//         console.log('You will never see this line.');
+//     }
+// }
+
+//Create a for loop that uses console.log to create the output shown below.
+
+var numberToStopAt = 2
+
+function minusFive () {
+
+        for (var i = 1; i < 5; i++) {
+
+                console.log('Loop counter is: ' + i);
+
+                if (i === numberToStopAt) {
+                        console.log('We have reached the stopping point: break!');
+                        // use the break keyword to exit from the while loop
+                        break;
+                }
+        }
+        console.log(minusFive())
+
 }
+
+minusFive();
 
 // if (evenOddPositiveNegative % 2 === 0){
 //     console.log("The number is even");
