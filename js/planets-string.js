15a79176e3fa2a161947dc4bd4a8ff2575bdc1ff
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-08-21 23:24:40 -0500

    Feat: completed first exercise for spli-join

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,53 +1,35 @@
 (function(){
     "use strict";
 
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    //var planetsArray;
+
     /**
      * TODO:
-     * Create an array of 4 people's names and store it in a variable called
-     * 'names'.
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
      */
 
-
-
-
-
-
+        console.log(planetsString);
+        var planetsArray = planetsString.split(" , ");
+        console.log(planetsArray);
 
     /**
      * TODO:
-     * Create a log statement that will log the number of elements in the names
-     * array.
-     */
-
-    /**
-     * TODO:
-     * Create log statements that will print each of the names individually by
-     * accessing each element's index.
-     */
-
-    /**
-     * TODO:
-     * Write some code that uses a for loop to log every item in the names
-     * array.
-     */
-
-    /**
-     * TODO:
-     * Refactor your above code to use a `forEach` loop
-     */
-
-    /**
-     * TODO:
-     * Create the following three functions, each will accept an array and
-     * return an an element from it
-     * - first: returns the first item in the array
-     * - second: returns the second item in the array
-     * - last: returns the last item in the array
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
      *
-     * Example:
-     *  > first([1, 2, 3, 4, 5]) // returns 1
-     *  > second([1, 2, 3, 4, 5]) // returns 2
-     *  > last([1, 2, 3, 4, 5]) // return 5
+     * BONUS:
+     * Create another string that would display your planets in an undordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
      */
+
+    var planetsString = planetsArray.join("<br>");
+    console.log(planetsString);
 
 })();
+
+// console.log(planetsString);
+// console.log(planetsArray);
