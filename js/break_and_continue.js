2268d779ea74bcbbf8eb4157e7a83b90c25f8342
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-08-22 20:31:14 -0500

    Feat: Attempt two with break/continue. I think i know what i need to do, but reread loop exercise again.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,34 +2,55 @@
 //     Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
 //     Your output should look like this:
 
+// var userInputNumber = prompt("Enter a ODD number between 1-50:")
+// function allOddNumbersToX (x) {
+//         if (userInputNumber > 50) {
+//             prompt("Enter an ODD number between 1-50:")
+//
+//     for (var i = 0; i <= 50; i++) {
+//         if (i % 2 === 0) {
+//
+//             continue;
+//         // if (i == userInputNumber);
+//            // console.log("Here is  an odd number: " +i);
+//
+//         } else if (i == userInputNumber)
+//             break;
+//             console.log("Here is an odd number: " +i);
+// //        else if (i == userInputNumber);
+//             continue;
+//         } else if (i == userInputNumber)
+//
+//         console.log("Skipping number: " + i);
+//
+//        // console.log("Here is  an odd number: " +i);
+//         //return console.log(userInputNumber);
+//
+// }
+//
+// allOddNumbersToX(userInputNumber);
+
+
 var userInputNumber = prompt("Enter a ODD number between 1-50:")
-function allOddNumbersToX (x) {
-        if (userInputNumber > 50) {
-            prompt("Enter an ODD number between 1-50:")
+//var numberToStopAt = userInputNumber;
+
+function allOdds(){
+    for (var i = 0; i <=50; i++) {
+        if (i % 2 !== 0) {
+            console.log('Odd number is: ' + i);
+        }else if (i === userInputNumber) {
+                console.log('You got: ' + userInputNumber)
+                // console.log('We have reached the stopping point: break!');
+                // // use the break keyword to exit from the while loop
+                break;
+                // nothing after the break will get processed
+                console.log('You will never see this.');
+            }
         }
-    for (var i = 0; i <= 50; i++) {
-        if (i % 2 === 0) {
-
-            continue;
-        // if (i == userInputNumber);
-           // console.log("Here is  an odd number: " +i);
-
-        } else if (i == userInputNumber)
-
-            console.log("Skipping number: " +i);
-//        else if (i == userInputNumber);
-        console.log("Here is  an odd number: " + i);
-
-       // console.log("Here is  an odd number: " +i);
-        //return console.log(userInputNumber);
-
     }
 
 
-}
-
-allOddNumbersToX(userInputNumber);
-
+allOdds();
 
 // var userInputNumber = prompt("Enter a ODD number between 1-50:")
 // function oddNumbers() {
