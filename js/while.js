e02a64d922f4582dbfd7bdaa5307f87c1e66e5ba
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-09-03 14:47:03 -0500

    created css login form

--- js/while.js
+++ js/while.js
@@ -1,14 +1,13 @@
-// "use strict";
-//
-// function timesTwo() {
-//     var num = 2;
-//
-//     while (num <= 65536) {
-//         num *= 2;
-//         console.log(num);
-//     }
-// }
-// timesTwo();
+"use strict";
 
+function timesTwo() {
+    var num = 2;
+
+    while (num <= 65536) {
+        num *= 2;
+        console.log(num);
+    }
+}
+timesTwo();
 
 
