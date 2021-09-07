447decdcc6ff025f223cc1fe72201f41d0e0f7c8
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-09-06 22:11:06 -0500

    tried redoing loops

---
+++ js/for_loops_redo.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// for (var i = 1; i < 100; i++) {
+//
+//     if (i % 2 !== 0) {
+//         // number isn't even
+//         // odd numbers aren't as cool
+//         // skip the rest of the loop and continue with the next iteration
+//         continue;
+//     }
+//
+//     console.log('Here is a lovely even number: ' + i);
+// }
+
+// function showMultiplicationTable(input){
+//     for (var i = 1; i <= 10; i++){
+//         console.log(input +" x "+ i +" = " +(input*i));
+//     }
+// }
+// showMultiplicationTable(6)
+
+
+// 123 is odd
+// 80 is even
+// 24 is even
+// 199 is odd
+// ...
+
+//var random = Math.floor(Math.random() * 200) + 20;
+// function randomNum(random){
+// var random = Math.floor(Math.random() * 200) + 20;
+//     for (var i = 1; i < 10; i++) {
+//         if (random % 2 !== 0 && random >= 21){
+//             console.log(random+  " is a ODD number.");
+//         } else
+//             console.log(random+  " is an even number.");
+//         //}
+//         //console.log(random);
+//     }
+// }
+// randomNum();
+
+//Create a for loop that uses console.log to create the output shown below.
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
