c47a3209c8ff321b53513ca1983bacab72f8b5d9
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-09-28 11:38:51 -0500

    completed jquery selectors exercise

--- js/jquery_intro_exercise.js
+++ js/jquery_intro_exercise.js
@@ -1,11 +1,13 @@
 "use strict";
 
-$(document).ready(function() {
-    alert("You have been alerted about DOM being aloaded.");
-});
+// $(document).ready(function() {
+//     alert("You have been alerted about DOM being aloaded.");
+// });
+//
+// var hello = $('#hello').html();
+// alert(hello);
+//
+// var ipsum = $('#ipsum').html();
+// alert(ipsum);
 
-var hello = $('#hello').html();
-alert(hello);
-
-var ipsum = $('#ipsum').html();
-alert(ipsum);
+$('.codeup').css('border', '1px solid red');
