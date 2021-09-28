999dd8ed2eeb22a2edd4b24d58bb49097f5fdbd1
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-09-28 13:47:01 -0500

    feat unsure if I commited earlier, so here is one more just in case

--- js/jquery_intro_exercise.js
+++ js/jquery_intro_exercise.js
@@ -1,13 +1,44 @@
 "use strict";
 
+// SETUP EXERCISE
 // $(document).ready(function() {
 //     alert("You have been alerted about DOM being aloaded.");
 // });
-//
+
+// ID SELECTORS EXERCISE
 // var hello = $('#hello').html();
 // alert(hello);
 //
 // var ipsum = $('#ipsum').html();
 // alert(ipsum);
 
-$('.codeup').css('border', '1px solid red');
+
+// CLASS SELECTORS EXERCISE
+// $('.lorem').css('border', '7px solid orangered');
+
+// Q4 Remove the class from one of the elements. Refresh and test that the border has been removed. YES THE BORDER AROUND THAT ELEMENT GETS REMOVED
+
+// Q5 Give another element an id of codeup. Does this element get a border now? NO, ONLY CLASSES WILL GET THE BORDER, NOT THE IDs
+
+
+// ELEMENT SELECTORS EXERCISE
+// $('li').css('font-size', '20px');
+//
+// $('h1, p, li').css('background-color', 'gray');
+
+// $('#yoyoHead').css('background-color', 'orangered').css('font-size', '50px'); JUST PLAYING AROUND ON THIS ONE
+
+// $('h1').ready(function() {
+//     alert("ALERTED H1.");
+// });
+
+// MULTIPLE SELECTORS EXERCISE
+
+
+
+
+
+// EXTRA STUFF SHOWN BY DORIAN
+// $('.greet').css('background-color', "orange").css("margin", '2px').html('<strong>Adieu</strong>') this is how deep you can gets.
+
+// $('*').css('border', '1px solid red').css('padding', '5px').css('margin', '5px'); this is the ALL SELECTOR
