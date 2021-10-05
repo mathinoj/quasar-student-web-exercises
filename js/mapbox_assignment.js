a3fdda006553c561b475c3eb6dea8dba180b0c22
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-10-05 12:29:52 -0500

    added array of three restaurants to mapbox exercise

--- js/mapbox_assignment.js
+++ js/mapbox_assignment.js
@@ -22,16 +22,15 @@ var map = new mapboxgl.Map(
 // SA GRID CORDS
 // 98.4936, 29.4241
 
-var marker = new mapboxgl.Marker({color:'orangered'})
+var marker = new mapboxgl.Marker({color: 'orangered'})
     .setLngLat([-98.48632214484502, 29.39340897528683])
     .setPopup(new mapboxgl.Popup().setHTML("<em><strong>CARNITAS LONJA</strong></em>"))
     .addTo(map)
 
 var rests = [
     {
-        "lngLat": .setLngLat([-98.48632214484502, 29.39340897528683])",
-        "popup": .setPopup(new mapboxgl.Popup().setHTML("<em><strong>CARNITAS LONJA</strong></em>")),
-
+        "lngLat": ".setLngLat([-98.48632214484502, 29.39340897528683])",
+        "popup": "large",
     }
 ]
 
@@ -39,3 +38,26 @@ var rests = [
 //     console.log(results);
 //     map.setLayerZoomRange('my-layer', 5, 20);
 // })
+
+// geocode('7959 Fredericksburg Rd Ste 151, San Antonio, TX 78229', firstMapboxLectureKey).then(function (results){
+//     map.setLayerZoomRange('my-layer', 10, 15)
+// })
+// wingstop
+
+var restaurants = [
+    {
+        "place": "Wingstop",
+        "food": "wings",
+        "location": [-98.570000459961685, 29.511746976534077],
+    },
+        {
+            "place": "Hon Machi",
+            "food": "Korean BBQ",
+            "location": [-98.58423889023796, 29.53839297877265],
+        },
+        {
+            "place": "2M",
+            "food": "BBQ",
+            "location": [-98.40468548399008, 29.387494627689],
+        }
+    ]
