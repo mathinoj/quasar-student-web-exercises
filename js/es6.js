e446a8d698a937db16f0286a4ae00acbf718c9b5
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-10-13 11:17:07 -0500

    continued work on es6 exercises

--- js/es6.js
+++ js/es6.js
@@ -26,12 +26,12 @@ const users = [
         name: 'justin',
         email: 'justin@codeup.com',
         languages: ['html', 'css', 'javascript', 'php']
-    },
-    {
-        name: 'matthias',
-        email: 'matthias@codeup.com',
-        languages: ['html', 'css', 'javascript']
     }
+    // {
+    //     name: 'matthias',
+    //     email: 'matthias@codeup.com',
+    //     languages: ['html', 'css', 'javascript']
+    // }
 ];
 
 // TODO: fill in your name and email and add some programming languages you know
@@ -42,6 +42,8 @@ const name = 'matthias';
 const email = 'matthias@codeup.com';
 const languages = ['html', 'css', 'javascript'];
 
+
+
 // TODO: rewrite the object literal using object property shorthand
 users.push({
     name: name,
@@ -49,11 +51,13 @@ users.push({
     languages: languages
 });
 
-const userz = {
-    name,
-    email,
-    languages: languages
-}
+users.push({name, email, languages});
+
+// const userz = {
+//     name,
+//     email,
+//     languages: languages
+// }
 
 
 // TODO: replace `var` with `let` in the following variable declarations
@@ -66,18 +70,22 @@ console.log(names);
 // users.forEach(function(user) {
 //   return emails.push(user.email);
 // });
+users.forEach((user: {...}) => emails.push(user.email));
+users.forEach(user => names.push(suer.name));
+console.log(emails);
+console.log(names);
 
-users.forEach = user => {
-    return emails.push(user.email);
-}
+// users.forEach = user => {
+//     return emails.push(user.email);
+// }
 
 // users.forEach(function(user) {
 //   return names.push(user.name);
 // });
 
-users.forEach = user => {
-    return names.push(user.name);
-}
+// users.forEach = user => {
+//     return names.push(user.name);
+// }
 
 // const sayHello = name => {
 //   const greeting = 'Hello,  ' + name + '!';
@@ -91,8 +99,8 @@ users.forEach = user => {
 // const sayHello = (name) => 'Hello, ' + name + '!';
 
 // TODO: replace `var` with `let` in the following declaration
-let developers = [];
-users.forEach(function (user) {
+// let developers = [];
+// users.forEach(function (user) {
     // TODO: rewrite the code below to use object destructuring assignment
     //       note that you can also use destructuring assignment in the function
     //       parameter definition
@@ -100,6 +108,10 @@ users.forEach(function (user) {
     const email = user.email;
     const languages = user.languages;
 
+    let developers = []
+    users.forEach(function(user: {...}){
+
+    }
 
     users.forEach = user => ({name, email, languages});
     {
@@ -152,7 +164,12 @@ developers.forEach(function (developer) {
 });
 list += '</ul>';
 
+for(let develoer of developers){
+    list += `<li>${developer}</li>`
+}
+list += '</ul>';
 
+// pass this in a div with id as "developers"
 // var shoppers = [
 //   {name: 'matt', amount: 100},
 //   {name: 'joe', amount: 200},
